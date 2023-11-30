9b136b79d0d51eb1c5393864563d2c2f2b1b9ad6
juanfrausto <juanfr97@hotmail.com>
2023-11-30 16:37:11 -0600

    Initial Commit

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Juan Frausto on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
